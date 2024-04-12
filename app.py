48e3cf35c6bb9f0868393d7182b0ae1b53f48ed9
Prajwal <prajwal7872@gmail.com>
2024-04-12 11:43:49 +0545

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+# import the model
+pipe = pickle.load(open('pipe.pkl','rb'))
+df = pickle.load(open('df.pkl','rb'))
+
+st.title("Laptop Predictor")
+
+# brand
+company = st.selectbox('Brand',df['Company'].unique())
+
+# type of laptop
+type = st.selectbox('Type',df['TypeName'].unique())
+
+# Ram
+ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
+
+# weight
+weight = st.number_input('Weight of the Laptop')
+
+# Touchscreen
+touchscreen = st.selectbox('Touchscreen',[0,1])
+
+# IPS
+ips = st.selectbox('IPS',[0,1])
+ppi = st.selectbox('PPI',[140,150])
+
+#cpu
+cpu = st.selectbox('CPU',df['Cpu brand'].unique())
+
+hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+
+ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+
+gpu = st.selectbox('GPU',df['Gpu brand'].unique())
+
+os = st.selectbox('OS',df['os'].unique())
+
+if st.button('Predict Price'):
+    
+    query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
+
+    query = query.reshape(1,12)
+    st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
